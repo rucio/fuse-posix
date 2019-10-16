7ed7b1ab253f690ba044831ec5fb52a1d9c2274e
Gabriele <sucre.91@hotmail.it>
2019-10-16 11:26:39 +0200

    Introducing Rucio API skeleton

---
+++ include/REST-API.h
@@ -0,0 +1,21 @@
+//
+// Created by Gabriele Gaetano Fronzé on 2019-10-16.
+//
+
+#ifndef RUCIO_FUSE_REST_API_H
+#define RUCIO_FUSE_REST_API_H
+
+#include <vector>
+#include <string>
+
+std::vector<std::string> rucio_list_scopes();
+
+std::vector<std::string> rucio_list_dids(const std::string& scope);
+
+std::vector<std::string> rucio_list_container_dids(const std::string& scope, const std::string& container_name);
+
+bool rucio_is_container(const std::string& path);
+
+void rucio_create_inode();
+
+#endif //RUCIO_FUSE_REST_API_H
---
+++ source/REST-API.cpp
@@ -0,0 +1,28 @@
+//
+// Created by Gabriele Gaetano Fronzé on 2019-10-16.
+//
+
+#include <REST-API.h>
+#include <curl-REST.h>
+#include <conn-par.h>
+#include <vector>
+#include <string>
+
+std::vector<std::string> rucio_list_scopes(){
+  auto curl_res = GET(rucio_server_url+"/scopes");
+  return curl_res.payload;
+}
+
+std::vector<std::string> rucio_list_dids(const std::string& scope){
+  auto curl_res = GET(rucio_server_url+"/dids/"+scope);
+  return curl_res.payload;
+}
+
+std::vector<std::string> rucio_list_container_dids(const std::string& scope, const std::string& container_name){
+  auto curl_res = GET(rucio_server_url+"/dids/"+scope+"/"+container_name); //????? Just guessing, not right!
+  return curl_res.payload;
+}
+
+bool rucio_is_container(const std::string& path){
+  return false;
+}
