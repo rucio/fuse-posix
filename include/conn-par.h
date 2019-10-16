7ea01264bf5c08477739af6ae3bd60a6c713388c
Gabriele <sucre.91@hotmail.it>
2019-10-16 11:24:57 +0200

    Introducing conn-par header

---
+++ include/conn-par.h
@@ -0,0 +1,14 @@
+//
+// Created by Gabriele Gaetano Fronzé on 2019-10-16.
+//
+
+#ifndef RUCIO_FUSE_CONNNECTION_PARAMETERS_H
+#define RUCIO_FUSE_CONNNECTION_PARAMETERS_H
+
+#include <curl/curl.h>
+#include <string>
+
+static std::string rucio_server_url = "";
+static int rucio_server_port = 80;
+
+#endif //RUCIO_FUSE_CONNNECTION_PARAMETERS_H
