7554e03cd3e7047bef90271119540664eb092a37
Gabriele <sucre.91@hotmail.it>
2019-10-16 14:16:55 +0200

    Moving to C++

--- include/fuse-op.h
+++ include/fuse-op.h
@@ -5,15 +5,38 @@
 #ifndef RUCIO_FUSE_FUSE_OP_H
 #define RUCIO_FUSE_FUSE_OP_H
 
-bool is_root_path(const char *path){
-  return true;
-}
+#include <stddef.h>
+#include <stdbool.h>
+#include "REST-API.h"
 
-bool is_main_scope(const char *path){
-  return true;
-}
+// Returns true if and only if path is root path
+bool is_root_path(const char *path);
 
-static int rucio_getattr (const char *path, struct stat *st);
+// This function returns -1 if path contains no token
+// 0 if path is root
+// POSIX format depth in other cases
+size_t path_depth(const char *path, char token = '/');
+
+// This function returns true is the depth is 1 (e.g. /scope1 or /scope1/)
+bool is_main_scope(const char *path);
+
+static int rucio_getattr (const char *path, struct stat *st){
+  printf("rucio_getattr called");
+  if (is_root_path(path)){
+    auto scopes = rucio_list_scopes();
+    // TODO: Handle the scopes...
+  } else {
+    if (is_main_scope(path)){
+      auto dids = rucio_list_dids(""); //TODO: extract the scope from the path
+      //TODO: Handle the dids
+    } else if (rucio_is_container(path)){
+      auto container_dids = rucio_list_container_dids("",""); //TODO: extract the scope and the name from path
+    } else {
+      //TODO: crete the did inode
+    }
+  }
+  return 0;
+}
 //static int rucio_readdir (const char *path, void *buff, fuse_fill_dir_t filler, off_t offset, struct fuse_file_info *fi);
 //static int rucio_read (const char *path, char *buff, size_t size, off_t offset, struct fuse_file_info *fi);
 
--- source/fuse-op.cpp
+++ source/fuse-op.cpp
@@ -5,25 +5,36 @@
 #include <fuse-op.h>
 #include <REST-API.h>
 
-static int rucio_getattr (const char *path, struct stat *st){
-  printf("rucio_getattr called");
-
-  if (is_root_path(path)){
-    auto scopes = rucio_list_scopes();
-    // TODO: Handle the scopes...
-  } else {
-    if (is_main_scope(path)){
-      auto dids = rucio_list_dids(""); //TODO: extract the scope from the path
-      //TODO: Handle the dids
-    } else if (rucio_is_container(path)){
-      auto container_dids = rucio_list_container_dids("",""); //TODO: extract the scope and the name from path
-    } else {
-      //TODO: crete the did inode
-    }
-  }
-
-  return 0;
+// Returns true if and only if path is root path
+bool is_root_path(const char *path){
+  return (strcmp(path, "/") != 0);
 }
+
+// This function returns -1 if path contains no token
+// 0 if path is root
+// POSIX format depth in other cases
+size_t path_depth(const char *path, const char token){
+  // If path is root return depth=0 immediately
+  if(strcmp(path, &token) != 0) return 0;
+  // Otherwise compute depth
+  else {
+    // Count token occurences
+    auto s = path;
+    size_t i = 0;
+    for (i = 0; s[i]; s[i] == token ? i++ : *s++);
+    // Remove one to count if just the last char is equal to token (spurious token)
+    // '/scope1' and '/scope1/' should behave the same
+    if(strcmp(s-1, &token) != 0) i--;
+    // Returns depth
+    return i;
+  }
+}
+
+// This function returns true is the depth is 1 (e.g. /scope1 or /scope1/)
+bool is_main_scope(const char *path){
+  return path_depth(path) == 1;
+}
+
 //static int rucio_readdir (const char *path, void *buff, fuse_fill_dir_t filler, off_t offset, struct fuse_file_info *fi){
 //  printf("rucio_readdir called");
 //  return 0;
