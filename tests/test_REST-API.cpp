51b1445805f11d9e86ccab63762efe78db833b55
Gabriele <sucre.91@hotmail.it>
2019-10-17 14:49:42 +0200

    Prettify test output

--- tests/test_REST-API.cpp
+++ tests/test_REST-API.cpp
@@ -3,10 +3,11 @@
 //
 
 #include <REST-API.h>
+#include <iostream>
 
 int main(){
 
-  for(auto const& line : rucio_get_auth_token_userpass()) printf("%s\n", line.c_str());
-
+  for(auto const& line : rucio_get_auth_token_userpass()) printf("%s", line.c_str());
+  std::cout << std::endl;
   return 0;
 }
