c2d76d5dc88a135b9d97c8168d1471631451ca16
Gabriele <sucre.91@hotmail.it>
2019-10-17 10:38:44 +0200

    Adding auth parameters to conn-par header

--- include/conn-par.h
+++ include/conn-par.h
@@ -11,4 +11,9 @@
 static std::string rucio_server_url = "";
 static int rucio_server_port = 80;
 
+static std::string rucio_account_name = "";
+static std::string rucio_user_name = "";
+static std::string rucio_password = "";
+
+
 #endif //RUCIO_FUSE_CONNNECTION_PARAMETERS_H
