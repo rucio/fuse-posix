1ddfec876fadba3334ea4908af693274c4022bbf
Gabriele <sucre.91@hotmail.it>
2019-10-17 11:33:29 +0200

    Adding auth token containers in common parameters

--- include/conn-par.h
+++ include/conn-par.h
@@ -7,6 +7,7 @@
 
 #include <curl/curl.h>
 #include <string>
+#include <chrono>
 
 static std::string rucio_server_url = "";
 static int rucio_server_port = 80;
@@ -15,5 +16,7 @@ static std::string rucio_account_name = "";
 static std::string rucio_user_name = "";
 static std::string rucio_password = "";
 
+static std::string rucio_conn_token_header = "";
+std::chrono::steady_clock::time_point rucio_conn_token_exp;
 
 #endif //RUCIO_FUSE_CONNNECTION_PARAMETERS_H
