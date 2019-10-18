1ed79c0fb092578e47841355f70cffb887916b7b
Gabriele <sucre.91@hotmail.it>
2019-10-18 13:52:42 +0200

    Updating FUSE skeleton

--- include/fuse-op.h
+++ include/fuse-op.h
@@ -13,22 +13,29 @@
 // This is where the rucio-compliant logic branching happens
 static int rucio_getattr (const char *path, struct stat *st){
   printf("rucio_getattr called");
-  // If at root list the available scopes
+  // If at root list the available servers
   if (is_root_path(path)){
-    auto scopes = rucio_list_scopes();
-    // TODO: Handle the scopes...
+    auto servers = rucio_list_servers();
+    //TODO: Render the available servers as directories
   } else {
-    // If is one of the top level scopes list the dids inside
-    if (is_main_scope(path)){
-      auto dids = rucio_list_dids(""); //TODO: extract the scope from the path
-      //TODO: Handle the dids
-    // Otherwise, if a container (or a dataset), list its dids
-    } else if (rucio_is_container(path)){
-      auto container_dids = rucio_list_container_dids("",""); //TODO: extract the scope and the name from path
-      //TODO: Handle dids taking care of loops!
-    // If it's a file just create the inode
-    } else {
-      //TODO: crete the did inode
+    std::string server_short_name = rucio_server_from_path(path);
+    // If at a server mountpoint just list the available scopes
+    if (is_server_mountpoint(path)) {
+      auto scopes = rucio_list_scopes(server_short_name);
+      // TODO: Handle the server scopes
+    }
+      // If is one of the top level scopes list the dids inside
+      if (is_main_scope(path)) {
+        auto dids = rucio_list_dids("", server_short_name); //TODO: extract the scope from the path
+        //TODO: Handle the dids
+      // Otherwise, if a container (or a dataset), list its dids
+      } else if (rucio_is_container(path)) {
+        auto container_dids = rucio_list_container_dids("", "", server_short_name); //TODO: extract the scope and the name from path
+        //TODO: Handle dids taking care of loops!
+      // If it's a file just create the inode
+      } else {
+        //TODO: create the did inode
+      }
     }
   }
   return 0;
