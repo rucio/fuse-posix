abe1ff2bee397519882bc05e13cc5a37aa542254
Gabriele <sucre.91@hotmail.it>
2019-10-30 22:16:51 +0100

    First partial implementation of FUSE methods

--- include/fuse-op.h
+++ include/fuse-op.h
@@ -7,40 +7,111 @@
 
 #include <stddef.h>
 #include <stdbool.h>
+#include <unistd.h>
 #include <REST-API.h>
 #include <utils.h>
 
-// This is where the rucio-compliant logic branching happens
 static int rucio_getattr (const char *path, struct stat *st){
   printf("rucio_getattr called");
-  // If at root list the available servers
-  if (is_root_path(path)){
-    auto servers = rucio_list_servers();
-    //TODO: Render the available servers as directories
-  } else {
-    std::string server_short_name = rucio_server_from_path(path);
-    // If at a server mountpoint just list the available scopes
-    if (is_server_mountpoint(path)) {
-      auto scopes = rucio_list_scopes(server_short_name);
-      // TODO: Handle the server scopes
-    }
+
+  st->st_uid = getuid();
+	st->st_gid = getgid();
+	st->st_atime = time( nullptr );
+	st->st_mtime = time( nullptr );
+
+	if ( is_root_path(path) )
+	{
+	  auto servers = rucio_list_servers();
+
+		st->st_mode = S_IFDIR | 0755;
+		st->st_nlink = 2 + servers.size();
+	}
+	else
+	{
+	  std::string server_short_name = rucio_server_from_path(path);
+
+	  if (is_server_mountpoint(path)) {
+	    auto scopes = rucio_list_scopes(server_short_name);
+
+      st->st_mode = S_IFDIR | 0755;
+		  st->st_nlink = 2 + scopes.size();
+	  } else {
       // If is one of the top level scopes list the dids inside
       if (is_main_scope(path)) {
-        auto dids = rucio_list_dids("", server_short_name); //TODO: extract the scope from the path
-        //TODO: Handle the dids
+        auto dids = rucio_list_dids(extract_scope(path), server_short_name);
+
+        st->st_mode = S_IFDIR | 0755;
+		    st->st_nlink = 2 + dids.size();
       // Otherwise, if a container (or a dataset), list its dids
       } else if (rucio_is_container(path)) {
-        auto container_dids = rucio_list_container_dids("", "", server_short_name); //TODO: extract the scope and the name from path
-        //TODO: Handle dids taking care of loops!
+        auto container_dids = rucio_list_container_dids(extract_scope(path), extract_name(path), server_short_name);
+
+        st->st_mode = S_IFDIR | 0755;
+		    st->st_nlink = 2 + container_dids.size();
       // If it's a file just create the inode
       } else {
-        //TODO: create the did inode
+        st->st_mode = S_IFREG | 0644;
+        st->st_nlink = 1;
+        st->st_size = 1024;
       }
     }
-  }
+	}
+
   return 0;
 }
-//static int rucio_readdir (const char *path, void *buff, fuse_fill_dir_t filler, off_t offset, struct fuse_file_info *fi);
-//static int rucio_read (const char *path, char *buff, size_t size, off_t offset, struct fuse_file_info *fi);
+
+static int rucio_readdir(const char *path, void *buffer, fuse_fill_dir_t filler, off_t offset, struct fuse_file_info *fi)
+{
+  filler(buffer, ".", nullptr, 0 );
+  filler(buffer, "..", nullptr, 0 );
+
+  if ( is_root_path(path) )	{
+    auto servers = rucio_list_servers();
+
+    for(auto const& server : servers){
+      filler(buffer, server.data(), nullptr, 0 );
+    }
+	}	else {
+	  std::string server_short_name = rucio_server_from_path(path);
+
+	  if (is_server_mountpoint(path)) {
+	    auto scopes = rucio_list_scopes(server_short_name);
+
+      for(auto const& scope : scopes){
+        filler(buffer, scope.data(), nullptr, 0 );
+      }
+	  } else {
+      // If is one of the top level scopes list the dids inside
+      if (is_main_scope(path)) {
+        auto dids = rucio_list_dids(extract_scope(path), server_short_name);
+
+        for(auto const& did : dids){
+          filler(buffer, did.name.data(), nullptr, 0 );
+        }
+      // Otherwise, if a container (or a dataset), list its dids
+      } else if (rucio_is_container(path)) {
+        auto container_dids = rucio_list_container_dids(extract_scope(path), extract_name(path), server_short_name);
+
+        //TODO: Handle dids taking care of loops!
+        for(auto const& did : container_dids){
+          filler(buffer, did.name.data(), nullptr, 0 );
+        }
+      }
+    }
+	}
+  return 0;
+}
+
+static int rucio_read(const char *path, char *buffer, size_t size, off_t offset, struct fuse_file_info *fi)
+{
+  if(not is_server_mountpoint(path) and not is_main_scope(path) and not rucio_is_container(path)){
+    //TODO: download file through Rucio
+    char *fileContent = nullptr;
+    memcpy( buffer, fileContent + offset, size );
+	  return strlen( fileContent ) - offset;
+  }
+
+  return -1;
+}
 
 #endif //RUCIO_FUSE_FUSE_OP_H
