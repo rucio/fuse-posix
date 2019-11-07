066dd7c11d1f943c63f5d1f594b86806066d3ae3
Gabriele <sucre.91@hotmail.it>
2019-11-07 16:39:16 +0100

    Flattening rucio_getattr if-elif-else tree

--- include/fuse-op.h
+++ include/fuse-op.h
@@ -25,56 +25,49 @@ static int rucio_getattr (const char *path, struct stat *st){
 	st->st_atime = time( nullptr );
 	st->st_mtime = time( nullptr );
 
-	if ( is_root_path(path) )
-	{
+	if ( is_root_path(path) ) {
 	  fastlog(DEBUG,"handling root path");
 	  auto servers = rucio_list_servers();
 
 		st->st_mode = S_IFDIR | 0755;
 		st->st_nlink = 2 + servers.size();
-	}
-	else
-	{
-	  if (is_server_mountpoint(path)) {
-	    fastlog(DEBUG,"handling server path");
-	    std::string server_short_name = extract_server_name(path);
-	    auto scopes = rucio_list_scopes(server_short_name);
+	} else if (is_server_mountpoint(path)) {
+    fastlog(DEBUG,"handling server path");
+    std::string server_short_name = extract_server_name(path);
+    auto scopes = rucio_list_scopes(server_short_name);
 
-      st->st_mode = S_IFDIR | 0755;
-		  st->st_nlink = 2 + scopes.size();
-	  } else {
-      // If is one of the top level scopes list the dids inside
-      if (is_main_scope(path)) {
-        fastlog(DEBUG,"handling scope path");
-        std::string server_short_name = extract_server_name(path);
-        auto dids = rucio_list_dids(extract_scope(path), server_short_name);
+    st->st_mode = S_IFDIR | 0755;
+    st->st_nlink = 2 + scopes.size();
+  // If is one of the top level scopes list the dids inside
+  } else if (is_main_scope(path)) {
+    fastlog(DEBUG,"handling scope path");
+    std::string server_short_name = extract_server_name(path);
+    auto dids = rucio_list_dids(extract_scope(path), server_short_name);
 
-        st->st_mode = S_IFDIR | 0755;
-		    st->st_nlink = 2 + dids.size();
-      // Otherwise, if a container (or a dataset), list its dids
-      } else if (rucio_is_container(path)) {
-        fastlog(DEBUG,"handling container path");
-        std::string server_short_name = extract_server_name(path);
-        auto container_dids = rucio_list_container_dids(extract_scope(path), extract_name(path), server_short_name);
+    st->st_mode = S_IFDIR | 0755;
+    st->st_nlink = 2 + dids.size();
+  // Otherwise, if a container (or a dataset), list its dids
+  } else if (rucio_is_container(path)) {
+    fastlog(DEBUG,"handling container path");
+    std::string server_short_name = extract_server_name(path);
+    auto container_dids = rucio_list_container_dids(extract_scope(path), extract_name(path), server_short_name);
 
-        fastlog(DEBUG,"DiDs: %d", container_dids.size());
-        auto nFiles = std::count_if(container_dids.begin(),container_dids.end(),[](const rucio_did& did){ return did.type == rucio_data_type::rucio_file; });
-        fastlog(DEBUG,"Files: %d", nFiles);
-        fastlog(DEBUG,"Containers/datasets: %d", container_dids.size() - nFiles);
+    fastlog(DEBUG,"DiDs: %d", container_dids.size());
+    auto nFiles = std::count_if(container_dids.begin(),container_dids.end(),[](const rucio_did& did){ return did.type == rucio_data_type::rucio_file; });
+    fastlog(DEBUG,"Files: %d", nFiles);
+    fastlog(DEBUG,"Containers/datasets: %d", container_dids.size() - nFiles);
 
-        st->st_mode = S_IFDIR | 0755;
-		    st->st_nlink = 2 + container_dids.size() - nFiles;
-      // If it's a file just create the inode
-      } else {
-        fastlog(DEBUG,"handling file path");
-        st->st_mode = S_IFREG | 0644;
-        st->st_nlink = 1;
-        st->st_size = 1024;
-      }
-    }
-	}
+    st->st_mode = S_IFDIR | 0755;
+    st->st_nlink = 2 + container_dids.size() - nFiles;
+  // If it's a file just create the inode
+  } else {
+    fastlog(DEBUG,"handling file path");
+    st->st_mode = S_IFREG | 0644;
+    st->st_nlink = 1;
+    st->st_size = 1024;
+  }
 
-  return 0;
+	return 0;
 }
 
 static int rucio_readdir(const char *path, void *buffer, fuse_fill_dir_t filler, off_t offset, struct fuse_file_info *fi)
@@ -92,6 +85,8 @@ static int rucio_readdir(const char *path, void *buffer, fuse_fill_dir_t filler,
     for(auto const& server : servers){
       filler(buffer, server.data(), nullptr, 0 );
     }
+
+    return 0;
 	}	else {
 	  std::string server_short_name = extract_server_name(path);
 
@@ -102,6 +97,8 @@ static int rucio_readdir(const char *path, void *buffer, fuse_fill_dir_t filler,
       for(auto const& scope : scopes){
         filler(buffer, scope.data(), nullptr, 0 );
       }
+
+      return 0;
 	  } else {
       // If is one of the top level scopes list the dids inside
       if (is_main_scope(path)) {
@@ -111,6 +108,8 @@ static int rucio_readdir(const char *path, void *buffer, fuse_fill_dir_t filler,
         for(auto const& did : dids){
           filler(buffer, did.name.data(), nullptr, 0 );
         }
+
+        return 0;
       // Otherwise, if a container (or a dataset), list its dids
       } else if (rucio_is_container(path)) {
         fastlog(DEBUG,"handling container path");
@@ -125,6 +124,8 @@ static int rucio_readdir(const char *path, void *buffer, fuse_fill_dir_t filler,
         for(auto const& did : container_dids){
           filler(buffer, did.name.data(), nullptr, 0 );
         }
+
+        return 0;
       }
     }
 	}
