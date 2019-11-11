2730a3b22b1c12b26183f3651ddcdfa84edff7e6
Gabriele <sucre.91@hotmail.it>
2019-11-11 18:35:32 +0100

    Fix

--- source/REST-API.cpp
+++ source/REST-API.cpp
@@ -277,7 +277,7 @@ std::vector<std::string> rucio_get_replicas_metalinks(const std::string& path){
   auto beg_pfn = rses.find('[');
   auto end_pfn = rses.find(']');
 
-  auto beg_rse = rses.find(", \"");
+  auto beg_rse = 0;
   auto end_rse = rses.find("\":", beg_rse + 1);
   while(beg_pfn != std::string::npos && end_pfn != std::string::npos){
     auto rse = std::string(rses.begin() + beg_rse + 1, rses.begin() + end_rse - 1);
@@ -286,7 +286,7 @@ std::vector<std::string> rucio_get_replicas_metalinks(const std::string& path){
     beg_pfn = rses.find('[', end_pfn + 1);
     end_pfn = rses.find(']', end_pfn + 1);
 
-    beg_rse = rses.find(", \"", end_pfn + 1);
+    beg_rse = end_pfn + 3;
     end_rse = rses.find("\":", beg_rse);
 
     fastlog(DEBUG, "---> %s : %s", rse.data(), pfn.data());
