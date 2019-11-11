1d7cde1489eb981195f687c579c5568fd41ce25b
Gabriele <sucre.91@hotmail.it>
2019-11-11 18:56:09 +0100

    Push

--- source/REST-API.cpp
+++ source/REST-API.cpp
@@ -274,32 +274,19 @@ std::vector<std::string> rucio_get_replicas_metalinks(const std::string& path){
 
   fastlog(DEBUG, "\n\nRSES:\n%s\n\n", rses.data());
 
-
-  auto beg_rse = rses.find('"');
-  auto end_rse = rses.find("\":", beg_rse + 1);
-  auto beg_pfn = rses.find('[', end_rse + 1);
+  auto beg_pfn = rses.find('[', 0);
   auto end_pfn = rses.find(']', beg_pfn + 1);
 
-  std::cout << beg_rse << std::endl;
-  std::cout << end_rse << std::endl;
   std::cout << beg_pfn << std::endl;
   std::cout << end_pfn << std::endl;
 
-  while(beg_pfn < rses.length() && end_pfn < rses.length() && beg_rse < rses.length() && end_rse < rses.length()){
-    auto rse = std::string(rses.begin() + beg_rse, rses.begin() + end_rse);
+  while(beg_pfn != std::string::npos && end_pfn != std::string::npos){
     auto pfn = std::string(rses.begin() + beg_pfn + 2, rses.begin() + end_pfn - 1);
 
-    beg_rse = rses.find('"', end_pfn);
-    end_rse = rses.find("\":", beg_rse + 1);
-    beg_pfn = rses.find('[', end_rse + 1);
+    beg_pfn = rses.find('[', end_pfn + 1);
     end_pfn = rses.find(']', beg_pfn + 1);
 
-    std::cout << beg_rse << std::endl;
-    std::cout << end_rse << std::endl;
-    std::cout << beg_pfn << std::endl;
-    std::cout << end_pfn << std::endl;
-
-    fastlog(DEBUG, "---> %s : %s", rse.data(), pfn.data());
+    fastlog(DEBUG, "---> %s", pfn.data());
   }
 
   return std::move(curl_res.payload);
