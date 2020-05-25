2b80169433b912272da71a351f04b6c6b01f19e6
Vivek Nigam <viveknigam.nigam3@gmail.com>
2020-05-25 22:26:34 +0530

    Added more Unit Tests for Utils #15

--- tests/gtest_utils.cpp
+++ tests/gtest_utils.cpp
@@ -4,6 +4,7 @@ Authors:
 */
 
 #include <utils.h>
+#include <vector>
 #include <gtest/gtest.h>
 
 using namespace std;
@@ -20,12 +21,7 @@ TEST(Utils_Test, Test_path_depth){
     EXPECT_EQ(1, path_depth("/a/",'/'));
     EXPECT_EQ(2, path_depth("/a/b",'/'));
     EXPECT_EQ(6, path_depth("/a/b/c/d/e/f/",'/'));
-    EXPECT_EQ(-1, path_depth("a", 'a'));
-}
-
-TEST(Utils_Test, Test_string_manipulation){
-    EXPECT_EQ("a", extract_server_name("/a/b/c"));
-    EXPECT_EQ("c", extract_scope("/a/b/c/d"));
+    EXPECT_EQ(0, path_depth("a", 'a'));
 }
 
 TEST(Utils_Test, Test_is_server_mountpoint){
@@ -46,10 +42,45 @@ TEST(Utils_Test, Test_is_main_scope){
     EXPECT_FALSE(is_main_scope("/server1/scope1/name1"));
 }
 
-// TEST(Utils_Test, Test_string_manipulation){
-//     string test_string = "/server/scope/container/dataset/filename/";
-//     EXPECT_NO_THROW(remove_trailing_token(test_string));
-// }
+TEST(Utils_Test, Test_string_manipulation){
+    string test_string = "/server/scope/container/dataset/filename/";
+    string exp_string = "/server/scope/container/dataset/filename";
+    remove_trailing_token(test_string);
+
+    EXPECT_EQ(test_string, exp_string);
+    EXPECT_EQ("server", extract_server_name(test_string));
+    EXPECT_EQ("scope", extract_scope(test_string));
+    EXPECT_EQ("filename", extract_name(test_string));
+    EXPECT_EQ("scope:filename", get_did(test_string));
+}
+
+TEST(Utils_Test, Test_structurize_DID){
+    std::string test_did = R"(
+        {
+            "scope": "user.root",
+            "type": "FILE",
+            "name": "test-19102019.txt",
+            "parent": null,
+            "level": 0
+        })";
+    std::vector<rucio_did> did_v;
+    structurize_did(test_did, did_v);
+    EXPECT_EQ("user.root", did_v[0].scope);
+    EXPECT_EQ(rucio_data_type::rucio_file, did_v[0].type);
+    EXPECT_EQ("test-19102019.txt", did_v[0].name);
+    EXPECT_EQ("null", did_v[0].parent);
+    EXPECT_EQ(0, did_v[0].level);
+}
+
+TEST(Utils_Test, Test_tokenize_python_list){
+    std::vector<std::string> expected_scopes = {"a","b","c","d"};
+    std::vector<std::string> scopes;
+    tokenize_python_list(R"(["a","b","c","d"])", scopes);
+    EXPECT_EQ("a", scopes[0]);
+    EXPECT_EQ("b", scopes[1]);
+    EXPECT_EQ("c", scopes[2]);
+    EXPECT_EQ("d", scopes[3]);
+}
 
 int main(int argc, char **argv) {
     testing::InitGoogleTest(&argc, argv);
