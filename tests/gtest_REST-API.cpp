5c50c73e039b9b8620e7e57d41e9067a9def576f
Vivek Nigam <viveknigam.nigam3@gmail.com>
2020-05-26 20:51:36 +0530

    Adding gtest_REST-API.cpp #15

--- tests/gtest_REST-API.cpp
+++ tests/gtest_REST-API.cpp
@@ -6,6 +6,16 @@
 using namespace std;
 
 
+std::string server_name;
+
+TEST(RESTAPI_Test, Test_server_connection){
+    parse_settings();
+    server_name = "rucio-dev-server";
+
+    EXPECT_NO_THROW(rucio_get_auth_token_userpass(server_name));
+    ASSERT_TRUE(rucio_is_token_valid(server_name));
+}
+
 
 int main(int argc, char **argv) {
     testing::InitGoogleTest(&argc, argv);
