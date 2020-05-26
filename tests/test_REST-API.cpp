063308b80e7ca3243d3c81c4139ba479c8f81b5a
Vivek Nigam <viveknigam.nigam3@gmail.com>
2020-05-26 21:54:14 +0530

    Fixed test_REST-API

--- tests/test_REST-API.cpp
+++ tests/test_REST-API.cpp
@@ -1,15 +1,10 @@
-/*
-Created by Gabriele Gaetano Fronzé on 2019-10-16.
-Authors:
-- Gabriele Gaetano Fronzé <gabriele.fronze at to.infn.it>, 2019
-- Vivek Nigam <viveknigam.nigam3@gmail.com>, 2020
-*/
+//
+// Created by Gabriele Gaetano Fronzé on 2019-10-17.
+//
 
 #include <REST-API.h>
 #include <globals.h>
 #include <iostream>
-
-using namespace fastlog;
 
 void test_server_connection(std::string server_short_name){
   printf("--------------------------------------------------------------------------------------------------------\n"
@@ -23,14 +18,14 @@ void test_server_connection(std::string server_short_name){
 
     auto token = get_server_token(server_short_name);
 
-    fastlog(INFO, "Token Received: %s", token->conn_token.c_str());
+    printf("Got the following token: %s\n", token->conn_token.c_str());
 
     if (not rucio_is_token_valid(server_short_name)) {
-      fastlog(ERROR, "Token Expired");
+      std::cout << "Token not valid!" << std::endl;
     } else {
       char buffer[100];
       strftime(buffer, 100, "%a, %d %b %Y %H:%M:%S %Z", &token->conn_token_exp);
-      fastlog(INFO, "Token will expire in %.2f hours", difftime(token->conn_token_exp_epoch, time(nullptr))/3600);
+      printf("Token will be valid until %s\n\n", buffer);
     }
   }
 }
@@ -73,17 +68,16 @@ void test_scope_dids(std::string server_short_name, std::string scope_name){
 }
 
 int main(){
-  parse_settings();
-  test_server_connection("rucio-dev-server");
-  //test_server_connection("rucio-dev-server");
+  test_server_connection("rucio-server-torino");
+  test_server_connection("rucio-server-ligo");
 
-  test_server_scopes("rucio-dev-server");
+  test_server_scopes("rucio-server-ligo");
 
-  auto scopes = rucio_list_scopes("rucio-dev-server");
+  auto scopes = rucio_list_scopes("rucio-server-ligo");
 
   if(not scopes.empty()){
     for(const auto& scope : scopes){
-      test_scope_dids("rucio-dev-server", scope);
+      test_scope_dids("rucio-server-ligo", scope);
     }
   } else {
     std::cout << "No scope found.\n";
@@ -93,16 +87,16 @@ int main(){
      "Testing is_container\n"
      "--------------------------------------------------------------------------------------------------------\n\n");
 
-  std::cout << "Container at /rucio-dev-server/user.root/test-ds " << rucio_is_container("/rucio-dev-server/user.root/test-ds") << " -> expected True\n";
-  std::cout << "Container at /rucio-dev-server/user.root/test2.txt " << rucio_is_container("/rucio-dev-server/user.root/test2.txt") << " -> expected False\n";
+  std::cout << "Container at /rucio-server-torino/user.root/test-ds " << rucio_is_container("/rucio-server-torino/user.root/test-ds") << " -> expected True\n";
+  std::cout << "Container at /rucio-server-torino/user.root/test2.txt " << rucio_is_container("/rucio-server-torino/user.root/test2.txt") << " -> expected False\n";
 
   printf("--------------------------------------------------------------------------------------------------------\n"
        "Retrieving dids from server %s, scope %s and name %s\n"
        "--------------------------------------------------------------------------------------------------------\n\n",
-       "rucio-dev-server",
+       "rucio-server-torino",
        "user.root",
        "test-ds");
-  auto ret = rucio_list_container_dids("user.root", "test-ds", "rucio-dev-server");
+  auto ret = rucio_list_container_dids("user.root", "test-ds", "rucio-server-torino");
   for(const auto& did : ret){
     std::cout << did.name << std::endl;
   }
